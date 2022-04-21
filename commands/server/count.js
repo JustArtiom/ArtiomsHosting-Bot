ed9301c101aba0127cd6af8145f6e5eb90532dd3
Artiom <83312438+JustArtiom@users.noreply.github.com>
2022-04-21 04:26:15 -0700

    Update count.js

--- commands/server/count.js
+++ commands/server/count.js
@@ -1,18 +1,22 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 module.exports = async (client, message, args) => {
-    if(!serverCount.get(message.author.id)) {
-        await serverCount.set(message.author.id, {
+
+    let user = message.mentions.users.first() ? message.mentions.users.first() : client.users.cache.get(args[1]) ? client.users.cache.get(args[1]) : client.users.cache.get(message.author.id);
+    if(!user) return message.channel.send(`This should not happend :/`);
+
+    if(!serverCount.get(user.id)) {
+        await serverCount.set(user.id, {
             used: 0,
             have: 3
-        })
-    }
+        });
+    };
 
     message.channel.send({
         embeds:[
             new Discord.MessageEmbed()
-            .setTitle(`${message.author.username}'s Server Count`)
+            .setTitle(`${user.username}'s Server Count`)
             .setColor(`#677bf9`)
-            .setDescription(`You used \`${serverCount.get(message.author.id).used}/${serverCount.get(message.author.id).have}\` servers`)
+            .setDescription(`You used \`${serverCount.get(user.id).used}/${serverCount.get(user.id).have}\` servers.`)
         ]
-    })
+    });
 }
