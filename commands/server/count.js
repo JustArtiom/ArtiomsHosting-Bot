0da42ec07a9a9c32e5b8cae4255be23a5f1bba1a
DIBSTER <101737992+DEV-DIBSTER1@users.noreply.github.com>
2022-04-21 06:40:08 -0400

    Allowing mentions and user ID's to check other people's server count.

--- commands/server/count.js
+++ commands/server/count.js
@@ -1,18 +1,22 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 module.exports = async (client, message, args) => {
-    if(!serverCount.get(message.author.id)) {
-        await serverCount.set(message.author.id, {
+    
+    let UserID = args[1] == null ? (message.author.id) : (args[1].match(/[0-9]{18}/).length == 0 ? args[1] : args[1].match(/[0-9]{18}/)[0]);
+    let User = await message.guild.members.cache.get(UserID);    
+
+    if(!serverCount.get(UserID)) {
+        await serverCount.set(UserID, {
             used: 0,
             have: 3
-        })
-    }
+        });
+    };
 
     message.channel.send({
         embeds:[
             new Discord.MessageEmbed()
-            .setTitle(`${message.author.username}'s Server Count`)
+            .setTitle(`${User.user.username}'s Server Count:`)
             .setColor(`#677bf9`)
-            .setDescription(`You used \`${serverCount.get(message.author.id).used}/${serverCount.get(message.author.id).have}\` servers`)
+            .setDescription(`You used \`${serverCount.get(message.author.id).used}/${serverCount.get(message.author.id).have}\` servers.`)
         ]
-    })
+    });
 }
