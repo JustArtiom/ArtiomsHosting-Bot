995728ada90a13b8650acd57baaa21326d5529be
Artiom <83312438+JustArtiom@users.noreply.github.com>
2022-05-22 17:06:59 +0100

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -19,7 +19,7 @@ module.exports = (client, message, newMessage) => {
     const embed = new Discord.MessageEmbed()
     embed.setTitle('✏️ Edited Message')
     embed.setColor(`BLUE`)
-    message.content ? embed.addField(`Message Content`, `\`\`\`\n${message.content}\`\`\``) : null
+    message.content ? embed.addField(`Message Content`, message.content.includes('```') ? `${message.content}` : `\`\`\`\n${message.content}\`\`\``)) : null
     message.attachments?.size !== 0 ? embed.setImage(message.attachments?.first()?.proxyURL) : null
     message.attachments?.size !== 0 ? content = message.attachments?.map(x => x?.proxyURL).join("\n") : content = null
     embed.setFooter({ text: `${message.member.user.tag} (${message.member.user.id}) \nin #${message.channel.name}`, iconURL: message?.member?.user?.displayAvatarURL()});
@@ -27,30 +27,4 @@ module.exports = (client, message, newMessage) => {
     
 
     client.channels.cache.get(config.channelID.messages_managering).send({content: content, embeds: [embed]}).catch(err => {})
-
-
-    // if (oldMessage.author == null || oldMessage.author.bot == true || !oldMessage.content || newMessage == null) return;
-
-    // let data = {
-    //     message: oldMessage.content,
-    //     member: oldMessage.member,
-    //     timestamp: Date.now(),
-    //     action: "edit"
-    // };
-
-    // if (client.snipes.get(oldMessage.channel.id) == null) client.snipes.set(oldMessage.channel.id, [data])
-    // else client.snipes.set(oldMessage.channel.id, [...client.snipes.get(oldMessage.channel.id), data]);
-
-    // client.snipes.set(oldMessage.channel.id, client.snipes.get(oldMessage.channel.id).filter(x => (Date.now() - x.timestamp) < 300000 && x != null));
-
-
-    // const embed = new Discord.MessageEmbed()
-    // .setThumbnail(oldMessage.author.avatarURL)
-    // .setColor("BLUE")
-    // .addField("Author ", `${oldMessage.author.tag} (ID: ${oldMessage.author.id})`)
-    // .addField("Message Content:", `${oldMessage.content === "" ?  "Message had no content" : oldMessage.content}`)
-    // .setTimestamp()
-    // .setFooter({text: "Message edited in " + oldMessage.channel.name});
-
-    // client.channels.cache.get(config.channelID.messages_managering).send({embeds:[embed]})
 }
