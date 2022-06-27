01263d66070d4a0eaf99c952439bc4c8c91df97a
SpaceDevelopmentDC <107330373+SpaceDevelopmentDC@users.noreply.github.com>
2022-06-27 13:09:02 +0800

    Update botmentioned.js

--- events/botmentioned.js
+++ events/botmentioned.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js')
 client.on('messageCreate', async(message) => {
 
   if(message.content == `<@${client.user.id}>` || message.content == `<@!${client.user.id}>`){
-    return message.reply(`**:wave: Hello __${message.author.username}__**,Im A Bot To Manage Servers And Create Server For You On **Artiom's Hosting!**\n ↳ Get Started using \`ah!help\` to see all my commands!`)
+    return message.reply(`**:wave: Hello __${message.author.username}__**, Im A Bot To Manage Servers And Create Server For You On **Artiom's Hosting!**\n ↳ Get Started using \`ah!help\` to see all my commands!`)
   }
 
 })
