3c4b13915a262c6e418a1ebdf19525a08fbf78cc
TheCtkHoster <69423422+TheCtkHoster@users.noreply.github.com>
2022-08-30 12:56:08 +0530

    Delete events directory

--- events/error.js
+++
@@ -1,6 +0,0 @@
-const config = require('../config.json')
-
-module.exports = async (client, error) => {
-    console.log("Error happend: "+error)
-    client.channels.cache.get(config.channelID.errorLog).send(''+error+'.')
-}
