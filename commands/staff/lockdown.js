e9a5e99900820878d41aa2a3ce05e3f56bca2e80
TheCtkHoster <69423422+TheCtkHoster@users.noreply.github.com>
2022-08-31 13:03:48 +0530

    Update lockdown.js

--- commands/staff/lockdown.js
+++ commands/staff/lockdown.js
@@ -1,9 +1,9 @@
-module.exports = async (client, message, args) => {
-    if(message.channel.permissionsFor(message.guild.id).has('SEND_MESSAGES')){
-        message.channel.permissionOverwrites.edit(message.channel.guild.roles.everyone, {SEND_MESSAGES: false });
-        message.channel.send(`:x: Oh no, covid came in this channel, time for a lockdown!`)
-    }else{
-        message.channel.permissionOverwrites.edit(message.channel.guild.roles.everyone, {SEND_MESSAGES: null });
-        message.channel.send(`:x: Channel unlocked!`)
-    }
-}
+        module.exports = async (client, message, args) => {
+            if(message.channel.permissionsFor(message.guild.id).has('SEND_MESSAGES')){
+                message.channel.permissionOverwrites.edit(message.channel.guild.roles.everyone, {SEND_MESSAGES: false });
+                message.channel.send(`:x: Oh no, monkeypox came in this channel, time for a lockdown!`)
+            }else{
+                message.channel.permissionOverwrites.edit(message.channel.guild.roles.everyone, {SEND_MESSAGES: null });
+                message.channel.send(`:x: Channel unlocked!`)
+            }
+        }
