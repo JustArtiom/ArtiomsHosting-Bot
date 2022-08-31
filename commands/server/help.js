8d04563efac5e770ef4a90cf0986e466d71bd921
TheCtkHoster <69423422+TheCtkHoster@users.noreply.github.com>
2022-08-31 12:55:01 +0530

    Update help.js

--- commands/server/help.js
+++ commands/server/help.js
@@ -3,7 +3,7 @@ module.exports = (client, message, args) => {
     message.channel.send({embeds:[
         new EmbedBuilder()
         .setTitle(`❓ | Need help?`)
-        .setColor(`RED`)
+        .setColor(`Red`)
         .setDescription(`\`AH!server count\` - shows how many server slots you have and used\n\`AH!server create\` - create a server\n\`AH!server delete\` - delete a server\n\`AH!server list\` - shows all your servers created\n\`AH!server status\` - allows you to interact with the server\n\`AH!server proxy\` - Proxy your server to your domain\n\`AH!server unproxy\` - Delete a proxy`)
     ]})
 }
