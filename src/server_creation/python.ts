0a40abc8cecf0aef28e7e58507c63d87804f3d20
JustArtiom <artiomsiatat@gmail.com>
2023-06-11 22:43:21 +0100

    bug: python server creation

--- src/server_creation/python.ts
+++ src/server_creation/python.ts
@@ -4,7 +4,7 @@ export default (userID: string, serverName: string, location: number[]) => {
         "user": userID,
         "nest": 5,
         "egg": 16,
-        "docker_image": "Python 3.11|ghcr.io/parkervcp/yolks:python_3.11",
+        "docker_image": "ghcr.io/parkervcp/yolks:python_3.11",
         "startup": 'if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi; if [[ ! -z {{PY_PACKAGES}} ]]; then pip install -U --prefix .local {{PY_PACKAGES}}; fi; if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi; /usr/local/bin/python /home/container/{{BOT_PY_FILE}}',
         "limits": {
             "memory": 1024,
