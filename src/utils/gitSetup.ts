b9d0699e52a20fc6b255a9603eb502d9900a1813
JustArtiom <artiomsiatat@gmail.com>
2023-06-13 23:32:17 +0100

    fix: git update bug

--- src/utils/gitSetup.ts
+++ src/utils/gitSetup.ts
@@ -1,6 +1,7 @@
 import { exec } from "node:child_process";
 import config from "../../config";
 import { log as consolelog } from "./console";
+import chalk from "chalk";
 
 export const runCommand = (cmd: string) => new Promise<string>((resolve, reject) => {
     exec(cmd, (error, stdout, stderr) => {
@@ -10,14 +11,15 @@ export const runCommand = (cmd: string) => new Promise<string>((resolve, reject)
 })
 
 export const gitSetup = async ({ log }: { log?: boolean } = {}) => {
-    const gitinit = await runCommand("git init").catch(() => {})
+    await runCommand("git init").catch(() => {})
     if(log) consolelog("Initializing .git folder repository")
     
-    if(!gitinit || gitinit.includes("Reinitialized existing Git repository in")) 
-        throw new Error("You already have initialised git. If you want to reinitiate git please delte \".git\" folder")
-    
     await runCommand(`git remote add origin ${config.settings.autoUpdate.github}`)
+    if(log) consolelog({name: "Github", description: `Remote origin set to ${config.settings.autoUpdate.github}`})
+
     await runCommand(`git checkout -b ${config.settings.autoUpdate.branch}`)
+    if(log) consolelog({name: "Github", description: `${config.settings.autoUpdate.branch} branch added`})
+    if(log) consolelog({name: "Github", description: `${chalk.green("Pass.")} git file has been successfully configured`})
 }
 
 export const gitPull = () => runCommand(`git pull origin ${config.settings.autoUpdate.branch}`)
